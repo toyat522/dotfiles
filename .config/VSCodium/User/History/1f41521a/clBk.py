82a72b907c8e17eeb791b99f27189d32738de728
Toya Takahashi <toyatakahashi522@gmail.com>
2024-05-27 13:40:03 -0700

    added vscode

---
+++ .config/VSCodium/User/History/1f41521a/clBk.py
@@ -0,0 +1,60 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.descriptions import ComposableNode
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    # zed
+    zed_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            get_package_share_directory(
+                "zed_wrapper"
+            ),
+            "/launch/obsolete/zed2i.launch.py"
+        ])
+    )
+
+    # mavros
+    mavros_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            get_package_share_directory(
+                "all_seaing_vehicle"
+            ),
+            "/launch/mavros.launch.py"
+        ])
+    )
+
+    # zed
+    zed_ld = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            get_package_share_directory(
+                "zed_wrapper"
+            ),
+            "/launch/obsolete/zed2i.launch.py"
+        ])
+    )
+
+    # pwm sender
+    pwm_sender_node = Node(
+        package="all_seaing_vehicle"
+    )
+
+    laserscan_node = Node(
+        package="velodyne_laserscan",
+        executable="velodyne_laserscan_node",
+        output="screen",
+    )
+
+
+    return LaunchDescription([
+        driver_container, 
+        transform_container,
+        laserscan_node,
+        mavros_ld,
+        zed_ld
+    ])
